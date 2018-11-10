49a11b1743cb7a430d531f5ef8ec3f2cca35e83f
Viktoriia <zhuravel.victoriia@gmail.com>
2018-11-10 15:20:03 +0100

    adds unitTests for primeNumberCheck

--- tests/unitTests.js
+++ tests/unitTests.js
@@ -1,10 +1,38 @@
 var assert = chai.assert;
 
 describe('Prime numbers', function() {
-    it('Should check prime numbers', function() {
+    it('should be false for numbers < 0', function() {
+        assert.isFalse(primeNumberCheck(-1));
+        assert.isFalse(primeNumberCheck(-9999999));
+    });
+    it('should be false for 0, 1', function() {
         assert.isFalse(primeNumberCheck(0));
         assert.isFalse(primeNumberCheck(1));
+    });
+    it('should be false for invalid input', function() {
+        assert.isFalse(primeNumberCheck("sadasd"));
+        assert.isFalse(primeNumberCheck());
+        assert.isFalse(primeNumberCheck(null));
+        assert.isFalse(primeNumberCheck(NaN));
+    });
+    it('should be false for numbers with floating point', function() {
+        assert.isFalse(primeNumberCheck(3.5));
+        assert.isFalse(primeNumberCheck(7.0000000001));
+    });
+
+    it('schould be true for M31', function() {
+        assert.isTrue(primeNumberCheck(Math.pow(2,31) - 1));
+    });
+
+    it('should be true for prime numbers', function() {
         assert.isTrue(primeNumberCheck(2));
-        assert.isTrue(primeNumberCheck(4));
-      });
+        assert.isTrue(primeNumberCheck(13));
+        assert.isTrue(primeNumberCheck(173));
+        assert.isTrue(primeNumberCheck(992093291443));
+    });
+    it('should be false for non-prime numbers', function() {
+        assert.isFalse(primeNumberCheck(15));
+        assert.isFalse(primeNumberCheck(331973133));
+        assert.isFalse(primeNumberCheck(992093291442));
+    });
   });
